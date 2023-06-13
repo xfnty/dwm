5b0b27e1b4f14887e5b6163db94d14493bc9d6a5
xfnty <tsentsevitsky.v@gmail.com>
2023-06-13 16:19:40 +0700

    Changed default layout to floating

--- config.def.h
+++ config.def.h
@@ -55,8 +55,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[]=",      tile },    /* first entry is default */
 	// { "[M]",      monocle },
     { NULL,       NULL },    /* cyclelayout() stop */
 };
